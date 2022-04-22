245bec3d85f30e4635e4fef03df437d609069eda
José Aquiles Guedes de Rezende <jjoseaquiless@gmail.com>
2022-04-22 16:44:03 -0300

    Refactoring: Extrair constante Mau-cheiro identificado: Números mágicos Descrição da refatoração: aplicou-se a refatoração para extrair constante para dar nome a todos os valores soltos no código. Classes/métodos/atributos afetados: * criado o atributo estático baseDeCalculo para poder extrair as constantes * atributo faixa1 até o atributo faixa4 substituido pelas constantes LIMITE_SUPERIOR_FAIXA_1 até LIMITE_SUPERIOR_FAIXA_4 * criado a constante ALIQUOTA_27_5 * criado a constante ALIQUOTA_22_5 * criado a constante ALIQUOTA_15 * criado a constante ALIQUOTA_75 * criado a constante RENDA_LIMITE_ANUAL_FAIXA_1 * criado a constante RENDA_LIMITE_ANUAL_FAIXA_2 * criado a constante RENDA_LIMITE_ANUAL_FAIXA_3 * criado a constante BASE_DE_CALCULO_FAIXA_1 * criado a constante BASE_DE_CALCULO_FAIXA_2 * criado a constante BASE_DE_CALCULO_FAIXA_3 * criado a constante BASE_DE_CALCULO_FAIXA_4

--- IRPF/src/IRPF/CalcularImposto.java
+++ IRPF/src/IRPF/CalcularImposto.java
@@ -1,36 +1,53 @@
 package IRPF;
 
 public class CalcularImposto {
-	private float faixa1 = 1903.98f;
-	private float faixa2 = 2826.65f;
-	private float faixa3 = 3751.05f;
-	private float faixa4 = 4664.68f;
+	private static float baseDeCalculo;
+	
+	private static final float RENDA_LIMITE_ANUAL_FAIXA_3 = 205.5667f;
+	private static final float RENDA_LIMITE_ANUAL_FAIXA_2 = 138.6600f;
+	private static final float RENDA_LIMITE_ANUAL_FAIXA_1 = 69.2003f;
+	
+	private static final float ALIQUOTA_27_5 = 0.275f;
+	private static final float ALIQUOTA_22_5 = 0.225f;
+	private static final float ALIQUOTA_15 = 0.15f;
+	private static final float ALIQUOTA_75 = 0.075f;
+
+	private static final float LIMITE_SUPERIOR_FAIXA1 = 1903.98f;
+	private static final float LIMITE_SUPERIOR_FAIXA2 = 2826.65f;
+	private static final float LIMITE_SUPERIOR_FAIXA3 = 3751.05f;
+	private static final float LIMITE_SUPERIOR_FAIXA4 = 4664.68f;
+
+	private static final float BASE_DE_CALCULO_FAIXA_4 = (baseDeCalculo - LIMITE_SUPERIOR_FAIXA4) * ALIQUOTA_27_5 + RENDA_LIMITE_ANUAL_FAIXA_1 + RENDA_LIMITE_ANUAL_FAIXA_2 + RENDA_LIMITE_ANUAL_FAIXA_3;
+	private static final float BASE_DE_CALCULO_FAIXA_3 = (baseDeCalculo - LIMITE_SUPERIOR_FAIXA3) * ALIQUOTA_22_5 + RENDA_LIMITE_ANUAL_FAIXA_1 + RENDA_LIMITE_ANUAL_FAIXA_2;
+	private static final float BASE_DE_CALCULO_FAIXA_2 = (baseDeCalculo - LIMITE_SUPERIOR_FAIXA2) * ALIQUOTA_15 + RENDA_LIMITE_ANUAL_FAIXA_1;
+	private static final float BASE_DE_CALCULO_FAIXA_1 = (baseDeCalculo - LIMITE_SUPERIOR_FAIXA1) * ALIQUOTA_75; 
 	
 	private IRPF _irpf;
 
 	CalcularImposto(IRPF source){
-		this._irpf = source;
+		_irpf = source;
+		baseDeCalculo = _irpf.getBaseDoCalculo();
 	}
 	
 	public float computar() {
-		if (_irpf.getBaseDoCalculo() <= faixa1 ) {
+		if (baseDeCalculo <= LIMITE_SUPERIOR_FAIXA1 ) {
 			_irpf.totalImposto = 0;
 			return _irpf.totalImposto;
 		}
-		else if (_irpf.getBaseDoCalculo() <= faixa2 ) {
-			_irpf.totalImposto = (_irpf.getBaseDoCalculo() - faixa1) * 0.075f;
+		else if (baseDeCalculo <= LIMITE_SUPERIOR_FAIXA2 ) {
+			_irpf.totalImposto = BASE_DE_CALCULO_FAIXA_1;
 			return _irpf.totalImposto;
 		}
-		else if (_irpf.getBaseDoCalculo() <= faixa3 ) {
-			_irpf.totalImposto = (_irpf.getBaseDoCalculo() - faixa2) * 0.15f + 69.2003f;
+		else if (baseDeCalculo <= LIMITE_SUPERIOR_FAIXA3 ) {
+			_irpf.totalImposto = BASE_DE_CALCULO_FAIXA_2;
 			return _irpf.totalImposto;
 		}
-		else if (_irpf.getBaseDoCalculo() <= faixa4 ) {
-			_irpf.totalImposto = (_irpf.getBaseDoCalculo() - faixa3) * 0.225f + 69.2003f + 138.6600f;
+		else if (baseDeCalculo <= LIMITE_SUPERIOR_FAIXA4 ) {
+			_irpf.totalImposto = BASE_DE_CALCULO_FAIXA_3;
 			return _irpf.totalImposto;
 		}
 		else {
-			_irpf.totalImposto = (_irpf.getBaseDoCalculo() - faixa4) * 0.275f + 69.2003f + 138.6600f + 205.5667f;
+			_irpf.totalImposto = BASE_DE_CALCULO_FAIXA_4;
 			return _irpf.totalImposto;
 		}
 	}
